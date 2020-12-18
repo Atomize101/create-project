b11b9241fd81937152f2dafa2824fba679a64c30
Chris Patrick <atomize101@gmail.com>
2020-12-18 09:37:18 -0500

    Creating template directories

--- src/main.js
+++ src/main.js
@@ -12,3 +12,28 @@ async function copyTemplateFiles(options) {
 		clobber: false,
 	});
 }
+
+export async function createProject(options) {
+	options = {
+		...options,
+		targetDirectory: options.targetDirectory || process.cwd(),
+	};
+	const currentFileUrl = import.meta.url;
+	const templateDir = path.resolve(
+		new URL(currentFileUrl).pathname,
+		'../../templates',
+		options.template.toLowerCase()
+	);
+	options.templateDirectory = templateDir;
+	try {
+		await access(templateDir, fs.constants.R_OK);
+	} catch (err) {
+		console.error('%s Invalid Template name', chalk.red.bold('ERROR'));
+		process.exit(1);
+	}
+	console.log('Copy project files');
+	await copyTemplateFiles(options);
+
+	console.log('%s Projeect ready', chalk.green.bold('DONE'));
+	return true;
+}
