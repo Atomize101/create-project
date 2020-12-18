0780e2dae8b843504b35b348fb9cc2a586f0b218
Chris Patrick <atomize101@gmail.com>
2020-12-18 09:29:15 -0500

    Building main.js

---
+++ src/main.js
@@ -0,0 +1,14 @@
+import chalk from 'chalk';
+import fs from 'fs';
+import ncp from 'ncp';
+import path from 'path';
+import { promisify } from 'util';
+
+const access = promisify(fs.access);
+const copy = promisify(ncp);
+
+async function copyTemplateFiles(options) {
+	return copy(options.templateDirectory, options.targetDirectory, {
+		clobber: false,
+	});
+}
