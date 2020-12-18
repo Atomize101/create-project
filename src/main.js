1d00204435b95af13e0bd1b6c0f57f1f150c1efb
Chris Patrick <atomize101@gmail.com>
2020-12-18 10:57:54 -0500

    Complete. Git now will initialize

--- src/main.js
+++ src/main.js
@@ -3,6 +3,9 @@ import fs from 'fs';
 import ncp from 'ncp';
 import path from 'path';
 import { promisify } from 'util';
+import execa from 'execa';
+import Listr from 'listr';
+import { projectInstall } from 'pkg-install';
 
 const access = promisify(fs.access);
 const copy = promisify(ncp);
@@ -11,6 +14,16 @@ async function copyTemplateFiles(options) {
 	return copy(options.templateDirectory, options.targetDirectory, {
 		clobber: false,
 	});
+}
+
+async function initGit(options) {
+	const result = await execa('git', ['init'], {
+		cwd: options.targetDirectory,
+	});
+	if (result.failed) {
+		return Promise.reject(new Error('Failed to initialize Git'));
+	}
+	return;
 }
 
 export async function createProject(options) {
@@ -29,8 +42,28 @@ export async function createProject(options) {
 		console.error('%s Invalid Template name', chalk.red.bold('ERROR'));
 		process.exit(1);
 	}
-	console.log('Copy project files');
-	await copyTemplateFiles(options);
+
+	const tasks = new Listr([
+		{
+			title: 'Copy project Files',
+			task: () => copyTemplateFiles(options),
+		},
+		{
+			title: 'Initialize Git',
+			task: () => initGit(options),
+			enabled: () => options.git,
+		},
+		{
+			title: 'Install dependencies',
+			task: () =>
+				projectInstall({
+					cwd: options.targetDirectory,
+				}),
+			skip: () => (!options.runInstall ? 'Pass --install to automatically install dependencies' : undefined),
+		},
+	]);
+
+	await tasks.run();
 
 	console.log('%s Projeect ready', chalk.green.bold('DONE'));
 	return true;
